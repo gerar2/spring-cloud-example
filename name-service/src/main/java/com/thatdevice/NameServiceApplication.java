fc6997836be368f3bea05feddbc5b1f274ad38fc
Gerardo González <gerardo@thatdevice.com>
2016-04-25 09:51:33 +0100

    Added Hystrix

--- name-service/src/main/java/com/thatdevice/NameServiceApplication.java
+++ name-service/src/main/java/com/thatdevice/NameServiceApplication.java
@@ -2,10 +2,12 @@ package com.thatdevice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class NameServiceApplication {
 
 	public static void main(String[] args) {
